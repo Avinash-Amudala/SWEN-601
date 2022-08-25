3674908abcd1c2625178783891c936316c85fbec
Avi9618 <77770336+Avi9618@users.noreply.github.com>
2022-08-25 06:30:36 +0530

    Initial commit

---
+++ Carroll.java
@@ -0,0 +1,17 @@
+//Name: of the class: Carroll
+//Name: Avinash Amudala
+//Email: aa9429@g.rit.edu
+//The main purpose of this assignment is to improve coding skills in java and practice with Git.
+//Declaring class Carroll
+public class Carroll {
+    public static void main(String[] args) {
+        //System.out is used multiple times keep text in a organised manner.
+        System.out.println("\"Begin at the ");
+        System.out.println("beginning,\", the King");
+        System.out.println("said, very gravely, \"and");
+        System.out.println("go on till you come to");
+        System.out.println("the end: then stop.\"");
+        System.out.println("          -Lewis Carroll");
+    }
+}
+//end of class Carroll
